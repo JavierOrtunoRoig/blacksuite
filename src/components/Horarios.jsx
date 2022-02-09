cabcc52e16de5478b891c2b42d1b49333bced9d5
Kelsier <javi_roig@hotmail.es>
2022-02-09 15:17:58 +0100

    Fix: Horario correcto

--- src/components/Horarios.jsx
+++ src/components/Horarios.jsx
@@ -12,37 +12,52 @@ export default function Horarios() {
           <div className={styles.dia}>
             <p>{translate('horariosLunes')}</p>
           </div>
-            <p>10:00 - 20:00</p>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+              <p style={{margin: '0'}}>10:00 - 13:00</p>
+              <p style={{margin: '0'}}>16:00 - 20:00</p>
+            </div>
         </div>
         <div className={styles['dia-container']}>
           <div className={styles.dia}>
             <p>{translate('horariosMartes')}</p>
           </div>
-            <p>10:00 - 20:00</p>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+              <p style={{margin: '0'}}>10:00 - 13:00</p>
+              <p style={{margin: '0'}}>16:00 - 20:00</p>
+            </div>
         </div>
         <div className={styles['dia-container']}>
           <div className={styles.dia}>
             <p>{translate('horariosMiercoles')}</p>
           </div>
-            <p>10:00 - 20:00</p>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+              <p style={{margin: '0'}}>10:00 - 13:00</p>
+              <p style={{margin: '0'}}>16:00 - 20:00</p>
+            </div>
         </div>
         <div className={styles['dia-container']}>
           <div className={styles.dia}>
             <p>{translate('horariosJueves')}</p>
           </div>
-            <p>10:00 - 20:00</p>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+              <p style={{margin: '0'}}>10:00 - 13:00</p>
+              <p style={{margin: '0'}}>16:00 - 20:00</p>
+            </div>
         </div>
         <div className={styles['dia-container']}>
           <div className={styles.dia}>
             <p>{translate('horariosViernes')}</p>
           </div>
-            <p>10:00 - 20:00</p>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+              <p style={{margin: '0'}}>10:00 - 13:00</p>
+              <p style={{margin: '0'}}>16:00 - 20:00</p>
+            </div>
         </div>
         <div className={styles['dia-container']}>
           <div className={styles.dia}>
             <p>{translate('horariosSabado')}</p>
           </div>
-            <p>10:00 - 13:00</p>
+            <p>10:00 - 14:00</p>
         </div>
         <div className={styles['dia-container']}>
           <div className={styles.dia}>
